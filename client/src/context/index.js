7b3e055cab3eb5a1b5a6b29d543b8a0fc376fd16
kyawphyothu <kyawphyothukpt256@gmail.com>
2023-05-31 05:30:18 +0630

    sidenavbar color change

--- client/src/context/index.js
+++ client/src/context/index.js
@@ -74,7 +74,7 @@ function MaterialUIControllerProvider({ children }) {
     miniSidenav: false,
     transparentSidenav: false,
     whiteSidenav: false,
-    sidenavColor: "info",
+    sidenavColor: "primary",
     transparentNavbar: true,
     fixedNavbar: true,
     openConfigurator: false,
