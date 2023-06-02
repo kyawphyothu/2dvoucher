f150d3a69423d6beedc37471fbdca1fb6ccab469
kyawphyothu <kyawphyothukpt256@gmail.com>
2023-06-02 11:21:07 +0630

    sample data update

--- tools/dataformat.js
+++ tools/dataformat.js
@@ -1,21 +1,21 @@
-const { ObjectId } = require("mongodb");
-const bcrypt = require("bcrypt");
+/** @format */
+
+const { ObjectId } = require('mongodb');
+const bcrypt = require('bcrypt');
 
 // let hash;
 // hash = await bcrypt.hash("password", 10);
 
-const hash = bcrypt.hashSync("password", 10);
-
+const hash = bcrypt.hashSync('password', 10);
 
 //users
-const users =
-[
+const users = [
 	{
 		_id: new ObjectId('646fa2ef0d7361b34925370a'),
 		name: 'Alice',
 		handle: 'alice',
 		password: hash,
-		role: 'superadmin'
+		role: 'superadmin',
 	},
 	{
 		_id: new ObjectId('646fa2f78b6443983ac9e6f2'),
@@ -38,8 +38,8 @@ const users =
 				type: '3d',
 				z: 500,
 				limit: 0,
-			}
-		]
+			},
+		],
 	},
 	{
 		_id: new ObjectId('646fa330bbcfdeb412db44bc'),
@@ -62,18 +62,16 @@ const users =
 				type: '3d',
 				z: 500,
 				limit: 0,
-			}
-		]
+			},
+		],
 	},
-
-]
+];
 
 //bettors
-const bettors =
-[
+const bettors = [
 	{
 		name: 'Bob',
-		handle: 'b_randomText',	//b_ mean bettors
+		handle: 'b_randomText', //b_ mean bettors
 		password: hash,
 		phone: '0912345678',
 		agentId: new ObjectId('646fa2f78b6443983ac9e6f2'),
@@ -84,10 +82,10 @@ const bettors =
 				name: 'မောင်ညွန့်',
 				date: new Date('2023-01-01'),
 				morning: true,
-				voucher: "43 200 /n 23.24.60 100 /n 45 100R200 /n 56R700 /n 9p100 /n 2468k200 /n os100 /n es200 /n 4t100 /n 5n200",
+				voucher: '43 200 /n 23.24.60 100 /n 45 100R200 /n 56R700 /n 9p100 /n 2468k200 /n os100 /n es200 /n 4t100 /n 5n200',
 				twoD: {
 					numbers: ['43', '23', '24', '60', '45', '54', '56R', '9P', '2468k', 'os', 'es', '4t', '5n'],
-					prices: [200, 100, 100, 100, 100, 200, 700, 100, 200, 100, 200, 100, 200]
+					prices: [200, 100, 100, 100, 100, 200, 700, 100, 200, 100, 200, 100, 200],
 				},
 				// note: '',
 				invest: 2400,
@@ -99,10 +97,10 @@ const bettors =
 				name: 'အောင်ထွေး',
 				date: new Date('2023-01-01'),
 				morning: false,
-				voucher: "43 200 /n 23.24.60 100 /n 45 100R200 /n 56R700 /n 9p100 /n 2468k200 /n os100 /n es200 /n 4t100 /n 5n200",
+				voucher: '43 200 /n 23.24.60 100 /n 45 100R200 /n 56R700 /n 9p100 /n 2468k200 /n os100 /n es200 /n 4t100 /n 5n200',
 				twoD: {
 					numbers: ['43', '23', '24', '60', '45', '54', '56R', '9P', '2468k', 'os', 'es', '4t', '5n'],
-					prices: [200, 100, 100, 100, 100, 200, 700, 100, 200, 100, 200, 100, 200]
+					prices: [200, 100, 100, 100, 100, 200, 700, 100, 200, 100, 200, 100, 200],
 				},
 				// note: '',
 				invest: 2400,
@@ -113,16 +111,16 @@ const bettors =
 				type: '3d',
 				name: 'စောမင်း',
 				date: new Date('2023-01-01'),
-				voucher: "012.200 /n 567r100",
+				voucher: '012.200 /n 567r100',
 				threeD: {
 					numbers: ['012', '567r'],
-					prices: [200, 100]
+					prices: [200, 100],
 				},
 				// note: '',
 				invest: 800,
 				win: ['012', 200], //only one
 				revenue: 100000,
-			}
+			},
 		],
 	},
 	{
@@ -134,10 +132,10 @@ const bettors =
 				type: 'mm2d',
 				date: new Date('2023-01-01'),
 				morning: true,
-				voucher: "48 200 /n 23.24.60 100 /n 45 100R200 /n 56R700 /n 9p100 /n 2468k200 /n os100 /n es200 /n 4t100 /n 5n200",
+				voucher: '48 200 /n 23.24.60 100 /n 45 100R200 /n 56R700 /n 9p100 /n 2468k200 /n os100 /n es200 /n 4t100 /n 5n200',
 				twoD: {
 					numbers: ['43', '23', '24', '60', '45', '54', '56R', '9P', '2468k', 'os', 'es', '4t', '5n'],
-					prices: [200, 100, 100, 100, 100, 200, 700, 100, 200, 100, 200, 100, 200]
+					prices: [200, 100, 100, 100, 100, 200, 700, 100, 200, 100, 200, 100, 200],
 				},
 				// note: '',
 				invest: 2400,
@@ -148,10 +146,10 @@ const bettors =
 				type: 'mm2d',
 				date: new Date('2023-01-01'),
 				morning: false,
-				voucher: "43 200 /n 23.24.60 100 /n 45 100R200 /n 56R700 /n 9p100 /n 2468k200 /n os100 /n es200 /n 4t100 /n 5n200",
+				voucher: '43 200 /n 23.24.60 100 /n 45 100R200 /n 56R700 /n 9p100 /n 2468k200 /n os100 /n es200 /n 4t100 /n 5n200',
 				twoD: {
 					numbers: ['43', '23', '24', '60', '45', '54', '56R', '9P', '2468k', 'os', 'es', '4t', '5n'],
-					prices: [200, 100, 100, 100, 100, 200, 700, 100, 200, 100, 200, 100, 200]
+					prices: [200, 100, 100, 100, 100, 200, 700, 100, 200, 100, 200, 100, 200],
 				},
 				// note: '',
 				invest: 2400,
@@ -161,80 +159,89 @@ const bettors =
 			{
 				type: '3d',
 				date: new Date('2023-01-01'),
-				voucher: "012.011.013.200 /n 567r100 ",
+				voucher: '012.011.013.200 /n 567r100 ',
 				threeD: {
 					numbers: ['012', '567r'],
-					prices: [200, 100]
+					prices: [200, 100],
 				},
 				// note: '',
 				invest: 800,
-				win: [['012', 200], ['011', 200], ['013', 100]], //only one
+				win: [
+					['012', 200],
+					['011', 200],
+					['013', 100],
+				], //only one
 				revenue: 115000,
-			}
+			},
 		],
-	}
-]
+	},
+];
 
 //win_numbers
-const win_numbers =
-[
+const win_numbers = [
 	{
 		type: 'mm2d',
-		date: new Date("2023-01-01"),
+		date: new Date('2023-01-01'),
 		morning: true,
 		number: 43,
 	},
 	{
 		type: 'mm2d',
-		date: new Date("2023-01-01"),
+		date: new Date('2023-01-01'),
 		morning: false,
 		number: 00,
 	},
 	{
 		type: '3d',
-		date: new Date("2023-01-01"),
+		date: new Date('2023-01-01'),
 		number: 666,
 	},
-
-]
+];
 
 //bet_types
-const bet_types =
-[
+const bet_types = [
 	{
 		type: 'mm2d',
+		name: 'MM 2D',
 	},
 	{
-		type: 'dubai2d',
+		type: 'du2d',
+		name: 'Dubai 2D',
 	},
 	{
-		type: '3d'
-	}
-]
+		type: 'ga2d',
+		name: 'Golden ACE 2D',
+	},
+	{
+		type: 'mg2d',
+		name: 'Mega 2D',
+	},
+	{
+		type: '3d',
+		name: '3D',
+	},
+];
 
 //user_types
-const user_types =
-[
+const user_types = [
 	{
 		type: 'superadmin',
 	},
 	{
-		type: 'agent'
+		type: 'agent',
 	},
 	{
-		type: 'subagent'
-	}
-]
+		type: 'subagent',
+	},
+];
 
 module.exports = {
 	users,
 	bettors,
 	bet_types,
 	user_types,
-	win_numbers
-}
-
-
+	win_numbers,
+};
 
 /*
 ဂဏာန်းနှစ်ခုကြား (.) ထည့်
